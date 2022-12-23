e38f8df116b4dcea78a598008da3ee6f11b29f6d
Oleg Budaev <t0r1n88@mail.ru>
2022-12-23 09:48:42 +0800

    Подготовка к

--- Cilian_main.py
+++ Cilian_main.py
@@ -10,16 +10,16 @@ from PyPDF2 import PdfMerger
 
 path_folder_data = 'data/'
 
-#
-# for dirpath, dirnames, filenames in os.walk(path_folder_data):
-#                     for filename in filenames:
-#                         if filename.endswith('.rtf'):
-#                             temp_str = dirpath.replace('/','\\')
-#                             fio = temp_str.split('\\')[1]
-#                             print(fio)
-#                             name_test = temp_str.split('\\')[3]
-#                             print(name_test)
-#                             shutil.copyfile(f'{dirpath}/{filename}', f'temp/{fio}_{name_test}.rtf')
+
+for dirpath, dirnames, filenames in os.walk(path_folder_data):
+                    for filename in filenames:
+                        if filename.endswith('.rtf'):
+                            temp_str = dirpath.replace('/','\\')
+                            fio = temp_str.split('\\')[1]
+                            print(fio)
+                            name_test = temp_str.split('\\')[3]
+                            print(name_test)
+                            shutil.copyfile(f'{dirpath}/{filename}', f'temp/{fio}_{name_test}.rtf')
 
 file_dct = dict()
 
